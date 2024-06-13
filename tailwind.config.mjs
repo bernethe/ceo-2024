f7dd63478fd2975e09f72210543fb0a7bcbe54fc
Harold Bernethe Soto González <bernethe@gmail.com>
2024-06-13 17:48:43 -0600

    feat: Add background image and update font family in tailwind.config
    chore: Update WhatsApp link in BtnScheduleAppointment
    
    chore: Update section tag in Help.astro
    
    chore: Add og:title and og:image meta tags in MainLayout.astro
    
    chore: Update social media links in Footer.astro
    
    chore: Update circle-grad.svg
    
    chore: Update FAQ section in FAQ.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'circle': "url('/src/assets/circle-grad.svg')"
+			},
 			fontFamily: {
 				'body': ['Montserrat Variable', 'sans-serif'],
 			},
